6bafd87bacbd02faad55bbdc82d44ab14c368297
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2020-12-10 12:27:38 +0100

    Legger på sjekk for å ignorerer tom samværsavtalel liste.

--- src/main/kotlin/no/nav/helse/prosessering/v1/PreprosseseringV1Service.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/PreprosseseringV1Service.kt
@@ -65,7 +65,7 @@ internal class PreprosseseringV1Service(
             )
         )
 
-        if (melding.type == Meldingstype.FORDELING && melding.fordeling != null) {
+        if (melding.type == Meldingstype.FORDELING && melding.fordeling != null && melding.fordeling.samværsavtale.isNotEmpty()) {
             komplettDokumentUrls.add(melding.fordeling.samværsavtale.map { it.toURI() })
         }
 
