803a0677d55021acfb3dd0557c96fc53c7188343
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-12-16 15:55:16 +0100

    Legger til metrikk over hvor vidt man har man har lastet opp samværsavtale eller ikke

--- src/main/kotlin/no/nav/helse/prosessering/v1/PreprossesertMeldingV1Metrics.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/PreprossesertMeldingV1Metrics.kt
@@ -1,10 +1,26 @@
 package no.nav.helse.prosessering.v1
 
+import io.prometheus.client.Counter
 import no.nav.helse.prosessering.v1.melding.PreprossesertMelding
 import org.slf4j.LoggerFactory
+
+private val generelCounter = Counter.build()
+    .name("generel_counter")
+    .help("Generel counter")
+    .labelNames("spm", "svar")
+    .register()
 
 internal fun PreprossesertMelding.reportMetrics(){
     val logger = LoggerFactory.getLogger("no.nav.helse.prosessering.v1.Metrics")
 
+    if(this.fordeling != null){
+        if(fordeling.samværsavtale.isEmpty()){
+            logger.info("Metrikk: Øker 'vedlegg' 'Nei'")
+            generelCounter.labels("vedlegg", "Nei").inc()
+        } else {
+            logger.info("Metrikk: Øker 'vedlegg' 'Ja'")
+            generelCounter.labels("vedlegg", "Ja").inc()
+        }
+    }
 
 }
