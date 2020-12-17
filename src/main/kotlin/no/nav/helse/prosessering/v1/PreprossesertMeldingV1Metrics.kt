1697c9f0307a4a4d67e4becaf753af4dc751f5fe
SanderOpperud <60642441+SanderOpperud@users.noreply.github.com>
2020-12-17 08:56:16 +0100

    Metrikk over hvor vidt man laster opp samværsavtale eller ikke (#15)
    * Legger til metrikk over hvor vidt man har man har lastet opp samværsavtale eller ikke

--- src/main/kotlin/no/nav/helse/prosessering/v1/PreprossesertMeldingV1Metrics.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/PreprossesertMeldingV1Metrics.kt
@@ -1,10 +1,21 @@
 package no.nav.helse.prosessering.v1
 
+import io.prometheus.client.Counter
 import no.nav.helse.prosessering.v1.melding.PreprossesertMelding
 import org.slf4j.LoggerFactory
+
+private val generellCounter = Counter.build()
+    .name("generell_counter")
+    .help("Generell counter")
+    .labelNames("spm", "svar")
+    .register()
 
 internal fun PreprossesertMelding.reportMetrics(){
     val logger = LoggerFactory.getLogger("no.nav.helse.prosessering.v1.Metrics")
 
+    if(this.fordeling != null){
+        if(fordeling.samværsavtale.isEmpty()) generellCounter.labels("vedlegg", "Nei").inc()
+        else generellCounter.labels("vedlegg", "Ja").inc()
+    }
 
 }
