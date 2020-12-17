0752cd0a747679a8c35157cd3c952729aeb40645
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-12-17 08:54:10 +0100

    typo

--- src/main/kotlin/no/nav/helse/prosessering/v1/PreprossesertMeldingV1Metrics.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/PreprossesertMeldingV1Metrics.kt
@@ -4,9 +4,9 @@ import io.prometheus.client.Counter
 import no.nav.helse.prosessering.v1.melding.PreprossesertMelding
 import org.slf4j.LoggerFactory
 
-private val generelCounter = Counter.build()
-    .name("generel_counter")
-    .help("Generel counter")
+private val generellCounter = Counter.build()
+    .name("generell_counter")
+    .help("Generell counter")
     .labelNames("spm", "svar")
     .register()
 
@@ -14,8 +14,8 @@ internal fun PreprossesertMelding.reportMetrics(){
     val logger = LoggerFactory.getLogger("no.nav.helse.prosessering.v1.Metrics")
 
     if(this.fordeling != null){
-        if(fordeling.samværsavtale.isEmpty()) generelCounter.labels("vedlegg", "Nei").inc()
-        else generelCounter.labels("vedlegg", "Ja").inc()
+        if(fordeling.samværsavtale.isEmpty()) generellCounter.labels("vedlegg", "Nei").inc()
+        else generellCounter.labels("vedlegg", "Ja").inc()
     }
 
 }
