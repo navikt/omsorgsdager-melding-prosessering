4d17d3411e815f4667e14e0a43264b945fb66650
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-01-06 11:10:41 +0100

    Oppdaterer dusseldorf-ktor og kotlin jvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val dusseldorfKtorVersion = "1.4.1.4754df6"
+val dusseldorfKtorVersion = "1.5.0.3315e68"
 val k9RapidBehovVersion = "1.91b665d"
 val k9RapidOverføreOmsorgsdagerVersion = "1.6d743f4"
 val k9RapidFordeleOmsorgsdagerVersion = "1.3265f25"
@@ -18,13 +18,13 @@ val handlebarsVersion = "4.1.2"
 val mainClass = "no.nav.helse.OmsorgsdagerMeldingProsesseringKt"
 
 plugins {
-    kotlin("jvm") version "1.3.70"
+    kotlin("jvm") version "1.4.21"
     id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 buildscript {
     // Henter ut diverse dependency versjoner, i.e. ktorVersion.
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/4e29fb7f5f69ab1b8d998fc1f674085e2e01e7ad/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/3315e68ec40ad9a24fa0c45d4f463d9cbae56fa0/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
